93605176e4e14cb270a2a6bb85706263234e299c
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-05-27 23:24:21 +0600

    auto alert option fixed

--- register.php
+++ register.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'headTagContainer.php';
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Register</title>
-</head>
 
 <body>
-
     <div class="w-25 bg-dark mx-auto mt-5 text-center text-white py-5 rounded">
         <h5>Sign Up</h5>
-
-        <form action="" method="post" class="mx-3">
-            <input type="text" name="name" id="" class="form-control mb-3 fw-bold" placeholder="Enter name">
-            <input type="text" class="form-control mb-3 fw-bold" name="email" placeholder="Enter the email">
-            <input type="password" class="form-control mb-3 fw-bold" name="password" placeholder="Enter the password">
+        <form action="" method="post" class="mx-3" onsubmit="return validateLoginForm()">
+            <input type="text" name="name" id="name" class="form-control mb-3 fw-bold" placeholder="Enter name" required>
+            <input type="text" class="form-control mb-3 fw-bold" name="email" id="email" placeholder="Enter the email" required>
+            <input type="password" class="form-control mb-3 fw-bold" name="password" id="password" placeholder="Enter the password" required>
             <input type="submit" class="btn btn-primary fw-bold" name="signup" value="Sign Up">
             <a href="./index.php" class="btn btn-success fw-bold text-white">Login</a>
-
         </form>
     </div>
     <?php
@@ -42,10 +32,9 @@
     }
     ?>
 
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
 </body>
+
 
 </html>
